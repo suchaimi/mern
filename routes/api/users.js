c6bc81614321d6ca06d6a58dbb83f684fe494fa3
suchaimi <suchaimi.afif@gmail.com>
2019-04-17 14:42:40 +0700

    Adding validation to register file

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,8 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
 // Load User Model
 const User = require("../../models/User");
 
@@ -22,13 +24,18 @@ router.get("/test", (req, res) =>
 // @desc    : Register Users Route
 // @access  : Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
-      return res.status(400).json({
-        email: "Email already exist !"
-      });
+      errors.email = "Email already exist !";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200",
@@ -93,7 +100,11 @@ router.get(
   "/current",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
-    res.json({ msg: "Success" });
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    });
   }
 );
 
